22be2b9560424323ea0bc96c788c919b7689f5a9
datahugs <galinabykova680@gmail.com>
2025-08-12 21:37:33 +0300

    aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -4,17 +4,17 @@ WITH leads_ranked AS (
         l.closing_reason,
         l.status_id,
         l.amount,
-        s.visitor_id,          
-        s.visit_date,          
+        s.visitor_id,
+        s.visit_date,
         s.source AS utm_source,
         s.medium AS utm_medium,
         s.campaign AS utm_campaign,
-           ROW_NUMBER() OVER (          
-            PARTITION BY l.lead_id    
+        ROW_NUMBER() OVER (
+            PARTITION BY l.lead_id
             ORDER BY ABS(EXTRACT(EPOCH FROM (l.created_at - s.visit_date))) ASC
         ) AS rn
-    FROM leads l
-    JOIN sessions s
+    FROM leads AS l
+    JOIN sessions AS s
         ON l.visitor_id = s.visitor_id
 ),
 leads_best AS (
@@ -33,66 +33,72 @@ leads_joined AS (
         lb.closing_reason,
         lb.status_id,
         lb.amount
-        FROM sessions s
-    LEFT JOIN leads_best lb
-        ON lb.visit_date = s.visit_date 
-        AND lb.utm_source = s.source
-        AND lb.utm_medium = s.medium
-        AND lb.utm_campaign = s.campaign
+    FROM sessions AS s
+    LEFT JOIN leads_best AS lb
+        ON
+            s.visit_date = lb.visit_date
+            AND s.source = lb.utm_source
+            AND s.medium = lb.utm_medium
+            AND s.campaign = lb.utm_campaign
 ),
-vk_costs as (
-select
-cast(date_trunc('day', va.campaign_date) as date) as visit_date,
-utm_source,
-utm_medium,
-utm_campaign,
-sum(daily_spent) as vk_cost
-from vk_ads as va
-group by 1,2,3,4
+vk_costs AS (
+    SELECT
+        va.utm_source,
+        va.utm_medium,
+        va.utm_campaign,
+        CAST(DATE_TRUNC('day', va.campaign_date) AS date) AS visit_date,
+        SUM(va.daily_spent) AS vk_cost
+    FROM vk_ads AS va
+    GROUP BY 1, 2, 3, 4
 ),
-ya_costs as (
-select 
-cast(date_trunc('day', ya.campaign_date) as date) as visit_date,
-utm_source,
-utm_medium,
-utm_campaign,
-sum(daily_spent) as ya_cost
-from ya_ads as ya
-group by 1,2,3,4
+ya_costs AS (
+    SELECT
+        ya.utm_source,
+        ya.utm_medium,
+        ya.utm_campaign,
+        CAST(DATE_TRUNC('day', ya.campaign_date) AS date) AS visit_date,
+        SUM(ya.daily_spent) AS ya_cost
+    FROM ya_ads AS ya
+    GROUP BY 1, 2, 3, 4
 )
-SELECT 
-    cast(date_trunc('day', lj.visit_date) as date) as visit_date,
+SELECT
+    CAST(DATE_TRUNC('day', lj.visit_date) AS date) AS visit_date,
     lj.utm_source,
     lj.utm_medium,
     lj.utm_campaign,
-    COUNT(lj.visitor_id) AS visitors_count, 
-    coalesce(vc.vk_cost,0::bigint) + coalesce(yc.ya_cost,0::bigint) as total_cost,
+    COUNT(lj.visitor_id) AS visitors_count,
+    COALESCE(vc.vk_cost, 0::bigint)
+    + COALESCE(yc.ya_cost, 0::bigint) AS total_cost,
     COUNT(lj.lead_id) AS leads_count,
-    COUNT(lj.lead_id) filter (where lj.closing_reason = 'Успешная продажа' or status_id = 142) as purchases_count,
-    coalesce(SUM(lj.amount) filter (where lj.closing_reason = 'Успешная продажа' or status_id = 142),0) as revenue
+    COUNT(lj.lead_id) FILTER (WHERE lj.closing_reason = 'Успешная продажа'
+        OR lj.status_id = 142) AS purchases_count,
+    COALESCE(SUM(lj.amount) FILTER (WHERE lj.closing_reason = 'Успешная продажа'
+        OR status_id = 142),0) AS revenue
 FROM leads_joined AS lj
-LEFT JOIN vk_costs as vc
-    on cast(date_trunc('day', lj.visit_date) as date) = vc.visit_date
-    AND lj.utm_source = vc.utm_source
-    AND lj.utm_medium = vc.utm_medium
-    AND lj.utm_campaign = vc.utm_campaign
-left JOIN ya_costs as yc
-    on cast(date_trunc('day', lj.visit_date) as date) = yc.visit_date
-    AND lj.utm_source = yc.utm_source
-    AND lj.utm_medium = yc.utm_medium
-    AND lj.utm_campaign = yc.utm_campaign
+LEFT JOIN vk_costs AS vc
+    ON 
+        CAST(DATE_TRUNC('day', lj.visit_date) AS DATE) = vc.visit_date
+        AND lj.utm_source = vc.utm_source
+        AND lj.utm_medium = vc.utm_medium
+        AND lj.utm_campaign = vc.utm_campaign
+LEFT JOIN ya_costs AS yc
+    ON
+        CAST(DATE_TRUNC('day', lj.visit_date) AS date) = yc.visit_date
+        AND lj.utm_source = yc.utm_source
+        AND lj.utm_medium = yc.utm_medium
+        AND lj.utm_campaign = yc.utm_campaign
 GROUP BY
-    cast(date_trunc('day', lj.visit_date) as date),
+    CAST(DATE_TRUNC('day', lj.visit_date) AS date),
     lj.utm_source,
     lj.utm_medium,
     lj.utm_campaign,
     vc.vk_cost,
-    ya_cost
-order by
-revenue desc nulls last,
-visit_date asc,
-visitors_count desc,
-lj.utm_source asc,
-lj.utm_medium asc,
-lj.utm_campaign asc
-limit 15;
+    yc.ya_cost
+ORDER BY
+    revenue DESC NULLS LAST,
+    visit_date ASC,
+    visitors_count DESC,
+    lj.utm_source ASC,
+    lj.utm_medium ASC,
+    lj.utm_campaign ASC
+LIMIT 15;
