0b1f0d7c3b3d67602606a1c7f39e83434cc8c687
datahugs <galinabykova680@gmail.com>
2025-08-15 19:22:05 +0300

    aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -10,47 +10,25 @@ WITH leads_ranked AS (
         s.medium AS utm_medium,
         s.campaign AS utm_campaign,
         ROW_NUMBER() OVER (
-            PARTITION BY l.lead_id
-            ORDER BY s.visit_date ASC
+            PARTITION BY s.visitor_id
+            ORDER BY s.visit_date DESC
         ) AS rn
-    FROM leads AS l
-    JOIN sessions AS s
-        ON l.visitor_id = s.visitor_id
-    WHERE s.visit_date < l.created_at
+    FROM sessions AS s
+    left JOIN leads AS l
+        ON s.visitor_id = l.visitor_id AND s.visit_date <= l.created_at 
+    WHERE s.medium != 'organic'
 ),
-
-leads_best AS (
+leads_joined AS (
     SELECT *
     FROM leads_ranked
     WHERE rn = 1
 ),
-
-leads_joined AS (
-    SELECT
-        s.visitor_id,
-        s.visit_date,
-        s.source AS utm_source,
-        s.medium AS utm_medium,
-        s.campaign AS utm_campaign,
-        lb.lead_id,
-        lb.closing_reason,
-        lb.status_id,
-        lb.amount
-    FROM sessions AS s
-    LEFT JOIN leads_best AS lb
-        ON
-            s.visit_date = lb.visit_date
-            AND s.source = lb.utm_source
-            AND s.medium = lb.utm_medium
-            AND s.campaign = lb.utm_campaign
-),
-
 vk_ya_costs AS (
     SELECT
         utm_source,
         utm_medium,
         utm_campaign,
-        CAST(DATE_TRUNC('day', campaign_date) AS date) AS visit_date,
+        CAST((campaign_date) AS date) AS visit_date,
         SUM(daily_spent) AS cost
     FROM (
         SELECT
@@ -71,14 +49,13 @@ vk_ya_costs AS (
     ) AS tab
     GROUP BY 1, 2, 3, 4
 )
-
 SELECT
-    CAST(DATE_TRUNC('day', lj.visit_date) AS date) AS visit_date,
+    CAST((lj.visit_date) AS date) AS visit_date,
     lj.utm_source,
     lj.utm_medium,
     lj.utm_campaign,
     COUNT(lj.visitor_id) AS visitors_count,
-    SUM(vyc.cost) AS total_cost,
+    vyc.cost AS total_cost,
     COUNT(lj.lead_id) AS leads_count,
     COUNT(lj.lead_id) FILTER (
         WHERE
@@ -92,15 +69,16 @@ SELECT
 FROM leads_joined AS lj
 LEFT JOIN vk_ya_costs AS vyc
     ON
-        CAST(DATE_TRUNC('day', lj.visit_date) AS date) = vyc.visit_date
+        cast((lj.visit_date) AS date) = vyc.visit_date
         AND lj.utm_source = vyc.utm_source
         AND lj.utm_medium = vyc.utm_medium
         AND lj.utm_campaign = vyc.utm_campaign
 GROUP BY
-    CAST(DATE_TRUNC('day', lj.visit_date) AS date),
+    CAST((lj.visit_date) AS date),
     lj.utm_source,
     lj.utm_medium,
-    lj.utm_campaign
+    lj.utm_campaign,
+    vyc.cost
 ORDER BY
     revenue DESC NULLS LAST,
     visit_date ASC,
